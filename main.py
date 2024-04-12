9759b19c396e3ff6fe3aaa0f978e1251769b5598
Jourzy <emmanora@hotmail.com>
2024-04-12 22:18:10 +0100

    Products are now displayed in rows with column headings.

--- main.py
+++ main.py
@@ -17,10 +17,31 @@ def get_welcome_page():
 
 
 
-# Show all products
-def show_all_products():
+# Get products
+def get_products():
     result = requests.get(
         'http://127.0.0.1:5000/products',
         headers= {'content-type': 'application/json'}
     )
     return result.json()
+
+
+def display_products(items):
+    # Print the names of the columns.
+    print("{:<4} {:<40} {:<60} {:<10} {:<10}".format(
+        'CODE', 'NAME', 'DESCRIPTION', 'PRICE', 'STOCK'))
+    print('-' * 125)
+
+    # print each data item.
+    for item in items:
+        print("{:<4} {:<40} {:<60} £{:<10} {:<10}".format(
+            item[0], item[1], item[2], item[3], item[4]
+        ))
+
+
+# get_welcome_page not working for me. Is it working for anyone else?
+# Wondering if it is because the data is not in json format?
+
+# get_welcome_page()
+products = get_products()
+display_products(products)
